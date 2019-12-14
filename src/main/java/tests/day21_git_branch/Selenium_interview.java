2ab7f02845496938e5e7911a50053b8c20223a32
esma <esmasaka5@gmail.com>
2019-12-14 11:42:37 -0500

    notes on branch

---
+++ src/main/java/tests/day21_git_branch/Selenium_interview.java
@@ -0,0 +1,19 @@
+package tests.day21_git_branch;
+
+public class Selenium_interview {
+    /*
+    How do you do peer/code review in your current project?
+Every team member pushes their code to their branch in GitHub.
+Then After testing the code himself, he/she creates a PR(Pull Request).
+Then we as a team sit and review the code and give feedback. If everything looks good and code is in the standards of our project and framework.
+Team lead merges the changes to master branch.
+After the master of GitHub is updated, all team member can now pull those changes to their local master branches.
+===================
+Best Practice based on our Git WorkFlow:
+--> commit all new changes to your local feature branch
+--> Do not commit to the local master
+--> Regularly Pull Remote master to your local master
+--> Merge new changes in your local master to your local feature branch
+==========================
+     */
+}
